e74158593b8347b126204958195e40e6c99b0010
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2020-10-08 16:22:14 +0300

    Add project files.

---
+++ SuggestionBoard.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SuggestionBoard.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ SuggestionBoard.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SuggestionBoard.Domain
+{
+    public class Class1
+    {
+    }
+}
